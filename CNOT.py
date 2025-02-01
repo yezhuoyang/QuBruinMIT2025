757dee62279118d21fb1351769099b8f64a76022
alexndros <alexandroutsos17@gmail.com>
2025-02-01 12:16:57 -0800

    CZ and CX gates

---
+++ CNOT.py
@@ -0,0 +1,13 @@
+from bloqade import move
+
+@move.vmove
+def cx_gate(state: move.core.AtomState, store_ind: int, gate_ind: int):
+    state.gate[[gate_ind]] = move.Move(state.storage[[store_ind]])
+    state = move.LocalRz(atom_state=state,phi=1,indices=gate_ind)
+    state = move.LocalXY(atom_state=state,x_exponent=-0.5,axis_phase_exponent=0.5,indicies=gate_ind)
+    state = move.GlobalCZ(atom_state=state)
+    state = move.LocalRz(atom_state=state,phi=1,indices=gate_ind)
+    state = move.LocalXY(atom_state=state,x_exponent=-0.5,axis_phase_exponent=0.5,indicies=gate_ind)
+    state.storage[[store_ind]] = move.Move(state.gate[[gate_ind]])
+    return state
+
