a3c1702c6fab189278cd52cc98e2b3ee90a8ad78
Keshavi Charde <keshavicharde1@gmail.com>
2025-02-01 11:29:12 -0800

    Keshavi changed Hadamard gate 2:30pm

--- hadamard.py
+++ hadamard.py
@@ -1,10 +1,10 @@
 # Hadamard Local
 @move.vmove()
-def Hadamard_local(state:move.core.AtomState, i):
-    state.gate[[i]] = move.Move(state.storage[[i]]) # Rz and Ry gates
-    state = move.LocalRz(atom_state=state,phi=1,indices=[i])
-    state = move.LocalXY(atom_state=state,x_exponent=-0.5,axis_phase_exponent=0.5,indices=[i])
-    state.storage[[i]] = move.Move(state.gate[[i]])
+def Hadamard_local(state:move.core.AtomState, i, j):
+    state.gate[[j]] = move.Move(state.storage[[i]]) # Rz and Ry gates
+    state = move.LocalRz(atom_state=state,phi=1,indices=[j])
+    state = move.LocalXY(atom_state=state,x_exponent=-0.5,axis_phase_exponent=0.5,indices=[j])
+    state.storage[[i]] = move.Move(state.gate[[j]])
     return state
 
 # Hadamard Global
