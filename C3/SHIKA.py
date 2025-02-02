45cc5e37af1a38b7d6d9717ac69cac5e00ca72fe
John ye <34168020+yezhuoyang@users.noreply.github.com>
2025-02-02 07:14:09 -0500

    Update SHIKA.py

--- C3/SHIKA.py
+++ C3/SHIKA.py
@@ -31,10 +31,10 @@ def main():
     state = move.LocalXY(atom_state=state,x_exponent=-1.0*pi,axis_phase_exponent=0.0,indices=t_ind)
 
 #Second CRz gates----
-    state.storage[[1]] = move.Move(state.gate[[2]])
+    state.storage[[2]] = move.Move(state.gate[[2]])
     state.storage[[0]] = move.Move(state.gate[[0]])
     state.gate[[2]] = move.Move(state.storage[[0]])
-    state.gate[[0]] = move.Move(state.storage[[1]])
+    state.gate[[0]] = move.Move(state.storage[2]])
     t_ind=[0,3]
     theta=0.15524282950959892
     beta=-1*pi+(theta/2)
@@ -45,12 +45,14 @@ def main():
     state = move.GlobalCZ(state)
     state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
     state = move.LocalXY(atom_state=state,x_exponent=-1.0*pi,axis_phase_exponent=0.0,indices=t_ind)
-    state.storage[[0,1]] = move.Move(state.gate[[1,2]])
+    state.storage[[1,2]] = move.Move(state.gate[[1,2]])
 
 #Third CRz gates----
-    state.gate[[2]] = move.Move(state.storage[[0]])
-    state.gate[[1]] = move.Move(state.storage[[1]])
-    t_ind=[1,3]
+    state.gate[[2]] = move.Move(state.storage[[1]])
+    state.gate[[1]] = move.Move(state.storage[[2]])
+    # 2013 after past 3 lines
+    
+    t_ind=[0,3]
     theta=0.15524282950959892
     beta=-1*pi+(theta/2)
     state = move.LocalRz(atom_state=state,phi=(beta),indices=t_ind)
@@ -65,7 +67,7 @@ def main():
     state = move.GlobalXY(atom_state=state,x_exponent=pi*0.1766811937,axis_phase_exponent=0.0*pi)
 
 #First parallel CRz-----
-    t_ind=[1,3]
+    t_ind=[0,3]
     theta=0.2858383611880559
     beta=-1*pi+(theta/2)
     state = move.LocalRz(atom_state=state,phi=(beta),indices=t_ind)
@@ -75,9 +77,24 @@ def main():
     state = move.GlobalCZ(state)
     state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
     state = move.LocalXY(atom_state=state,x_exponent=-1.0*pi,axis_phase_exponent=0.0,indices=t_ind)
-    state.storage[[0,2]] = move.Move(state.gate[[0,2]])
+    state.storage[[1,2]] = move.Move(state.gate[[1,2]])
 
 #Second CRz gates----
+    state.gate[[1]] = move.Move(state.storage[[2]])
+    state.gate[[2]] = move.Move(state.storage[[1]])
+    t_ind=[0,3]
+    theta=0.2858383611880559
+    beta=-1*pi+(theta/2)
+    state = move.LocalRz(atom_state=state,phi=(beta),indices=t_ind)
+    state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
+    state = move.GlobalCZ(state)
+    state = move.LocalXY(atom_state=state,x_exponent=-theta/2,axis_phase_exponent=0.0,indices=t_ind)
+    state = move.GlobalCZ(state)
+    state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
+    state = move.LocalXY(atom_state=state,x_exponent=-1.0*pi,axis_phase_exponent=0.0,indices=t_ind)
+
+#Third CRz gates----
+    state.storage[[0,2]] = move.Move(state.gate[[0,2]])
     state.gate[[0]] = move.Move(state.storage[[2]])
     state.gate[[2]] = move.Move(state.storage[[0]])
     t_ind=[1,3]
@@ -90,22 +107,7 @@ def main():
     state = move.GlobalCZ(state)
     state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
     state = move.LocalXY(atom_state=state,x_exponent=-1.0*pi,axis_phase_exponent=0.0,indices=t_ind)
-    state.storage[[0,1,2]] = move.Move(state.gate[[0,1,2]])
-
-#Third CRz gates----
-    state.gate[[0]] = move.Move(state.storage[[2]])
-    state.gate[[1,2]] = move.Move(state.storage[[0,1]])
-    t_ind=[1,3]
-    theta=0.2858383611880559
-    beta=-1*pi+(theta/2)
-    state = move.LocalRz(atom_state=state,phi=(beta),indices=t_ind)
-    state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
-    state = move.GlobalCZ(state)
-    state = move.LocalXY(atom_state=state,x_exponent=-theta/2,axis_phase_exponent=0.0,indices=t_ind)
-    state = move.GlobalCZ(state)
-    state = move.LocalXY(atom_state=state,x_exponent=-0.5*pi,axis_phase_exponent=0.5*pi,indices=t_ind)
-    state = move.LocalXY(atom_state=state,x_exponent=-1.0*pi,axis_phase_exponent=0.0,indices=t_ind)
-    state.storage[[0,1,2,3]] = move.Move(state.gate[[2,1,0,3]])
+    state.storage[[0,1,2,3]] = move.Move(state.gate[[0,1,2,3]])
 
     
 #Rx Global 2---------
